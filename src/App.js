63d4d3e094128f3a64e2604a3448c26b99138e26
SeraySayan <seraysayan00@gmail.com>
2022-11-19 22:26:21 +0200

    Header
    added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 
+function Header(props) {
+    console.log(props);
+    return (
+        <header>
+            <h1>{props.title}</h1>
+        </header>
+    );
+}
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Header title="mal seray" />
+        </div>
+    );
 }
 
 export default App;
