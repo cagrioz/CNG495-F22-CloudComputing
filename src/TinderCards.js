d881334882c73516cc21210c9480caa8adb31c19
SeraySayan <seraysayan00@gmail.com>
2022-11-23 15:41:03 +0200

    tinder card

---
+++ src/TinderCards.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import TinderCard from 'react-tinder-card';
+import F1 from './cagridogu.jpg';
+import F2 from './seray.jpeg';
+import './TinderCards.css';
+
+function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name: 'Seray Sayan',
+            url: F2,
+        },
+        {
+            name: 'Doğu Çağrı Özarpacı',
+            url: F1,
+        },
+    ]);
+
+    return (
+        <div>
+            <h1>Tinder Card</h1>
+            {people.map((person) => (
+                <TinderCard className="swipe" key={person.name}>
+                    <div className="card" style={{ backgroundImage: `url(${person.url})` }}>
+                        <h3>{person.name}</h3>
+                    </div>
+                </TinderCard>
+            ))}
+        </div>
+    );
+}
+
+export default TinderCards;
