78e1bf6a9b9650c1d0b1cce631f9d3d32307d941
SeraySayan <seraysayan00@gmail.com>
2022-12-02 13:44:10 +0200

    lastTinderCard

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -19,13 +19,15 @@ function TinderCards() {
     return (
         <div>
             <h1>Tinder Card</h1>
-            {people.map((person) => (
-                <TinderCard className="swipe" key={person.name}>
-                    <div className="card" style={{ backgroundImage: `url(${person.url})` }}>
-                        <h3>{person.name}</h3>
-                    </div>
-                </TinderCard>
-            ))}
+            <div className="tindeCards_cardContainer">
+                {people.map((person) => (
+                    <TinderCard className="swipe" key={person.name} preventSwipe={['up', 'down']}>
+                        <div className="card" style={{ backgroundImage: `url(${person.url})` }}>
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     );
 }
