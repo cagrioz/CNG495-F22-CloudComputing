4fadfb0e1a6a0ead70a96a1654f517c7defd78cf
SeraySayan <seraysayan00@gmail.com>
2023-01-14 19:53:50 +0200

    mapButton added

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import PersonIcon from '@mui/icons-material/Person';
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 import IconButton from '@mui/material/IconButton';
+import MapIcon from '@mui/icons-material/Map';
 import Logo from './logotinder.png';
 import './Header.css';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import { Box } from '@mui/material';
 
 function Header({ backButton }) {
     const history = useHistory();
@@ -27,11 +27,18 @@ function Header({ backButton }) {
                 <img className="header__logo" src={Logo} alt="tinder-logo" />
             </Link>
 
-            <Link to="/chat">
-                <IconButton>
-                    <QuestionAnswerIcon className="header__icon" fontSize="large" />
-                </IconButton>
-            </Link>
+            <div className="button">
+                <Link to="/chat">
+                    <IconButton>
+                        <QuestionAnswerIcon className="header__icon" fontSize="large" />
+                    </IconButton>
+                </Link>
+                <Link to="/map">
+                    <IconButton>
+                        <MapIcon className="header__icon" fontSize="large" />
+                    </IconButton>
+                </Link>
+            </div>
         </div>
     );
 }
