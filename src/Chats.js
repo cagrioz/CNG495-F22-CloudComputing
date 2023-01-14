21cd9b98515374d93d181b5c8c7b1d2c22db6534
Cagri Ozarpaci <devcagri@gmail.com>
2023-01-15 00:56:43 +0200

    basic styling and db calls

--- src/Chats.js
+++ src/Chats.js
@@ -3,8 +3,25 @@ import Chat from './Chat';
 
 import profilePic from './cagridogu.jpg';
 import profilePic2 from './seray.jpeg';
+import { useEffect, useState } from 'react';
+import database from './firebase';
 
-function Chats() {
+function Chats({ user }) {
+    // Make a request to the database to get all the chats
+    const [chats, setChats] = useState(null);
+
+    useEffect(() => {
+        // Make request to the database to chat collection that either user1 or user2 is equal to the logged in user
+        database
+            .collection('chats')
+            .where('users', 'array-contains', user.id)
+            .onSnapshot((snapshot) => {
+                setChats(snapshot.docs.map((doc) => doc.data().messages));
+            });
+
+        console.log(chats);
+    }, [user.id]);
+
     return (
         <div className="chats">
             <Chat name="Amy Acker" message="How r u?" timestamp="5 mins ago" profilePic={profilePic} />
