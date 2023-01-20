98ca036ae4c5e8a60c99fa54e2917d334014f7e8
Cagri Ozarpaci <devcagri@gmail.com>
2023-01-20 15:46:17 +0200

    OnSwipe event

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,12 +1,19 @@
 import TinderCard from 'react-tinder-card';
 import './TinderCards.css';
 
-function TinderCards({ people }) {
+function TinderCards({ people, setPeople }) {
+    const onSwipe = (direction) => {
+        if (direction === 'left' || direction === 'right') {
+            // Remove the last person from people array
+            setPeople(people.slice(0, -1));
+        }
+    };
+
     return (
         <div style={{ position: 'relative' }}>
             <div className="tindeCards_cardContainer">
                 {people.map((person) => (
-                    <TinderCard className="swipe" key={person.name} preventSwipe={['up', 'down']}>
+                    <TinderCard className="swipe" key={person.name} onSwipe={onSwipe} preventSwipe={['up', 'down']}>
                         <div className="card" style={{ backgroundImage: `url(${person.url})` }}>
                             <h3>{person.name}</h3>
                             <h4>{person.age}</h4>
